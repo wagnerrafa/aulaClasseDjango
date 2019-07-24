1e1ece747aab47754c8a7d2d25fd910fde06b706
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-07-24 11:08:23 -0300

    aula de classeObjeto

---
+++ poo.py
@@ -0,0 +1,23 @@
+# class Pessoa:
+#     def __init__(self, nome, idade):
+#         self.nome = nome
+#         self.idade = idade
+#         print(nome)
+#         print(idade)
+#     def quem_sou(self):
+#         return "quem sou eu" + self.nome
+#     def _dormir(self):
+#         return "zzzzz"
+
+# class Pedreiro:
+#     def __init__(self,nome,idade,marmita):
+#         super().__init__nome__idade
+#         self.marmita
+
+# wagner = Pessoa("Wagner", 25)
+
+class Mate():
+ 
+    @staticmethod
+    def area(x, y):
+        return x*y
